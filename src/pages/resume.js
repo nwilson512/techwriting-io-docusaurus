f2dc44c6bb75bd536ee981fa596135d28df7f5ad
nathan <nathan.wilson.512@gmail.com>
2021-09-20 23:34:22 -0700

    init resume react component

---
+++ src/pages/resume.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ResumeRender from "../components/ResumeRender"
+
+export default function render() {
+    return(
+        <>
+            <ResumeRender />
+        </>
+    )
+}
