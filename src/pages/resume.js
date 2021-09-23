fbd4d4b2c800261db776d76272eac22c1f8e494f
nathan <nathan.wilson.512@gmail.com>
2021-09-22 19:19:28 -0700

    Don't need to import that component

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,4 @@
 import React from "react"
-import ResumeRender from "../components/ResumeRender"
 import ResumeEducation from "../components/ResumeEducation"
 import ResumeSkills from "../components/ResumeSkills"
 import ResumeExperience from "../components/ResumeExperience"
