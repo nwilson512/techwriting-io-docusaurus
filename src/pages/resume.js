544011efd69e347f0308733113815143a297720c
nathan <nathan.wilson.512@gmail.com>
2021-09-22 20:06:32 -0700

    add some styling to the resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,13 +2,18 @@ import React from "react"
 import ResumeEducation from "../components/ResumeEducation"
 import ResumeSkills from "../components/ResumeSkills"
 import ResumeExperience from "../components/ResumeExperience"
+import Layout from '@theme/Layout';
 
 export default function render() {
     return(
         <>
-            <ResumeExperience />
-            <ResumeEducation />
-            <ResumeSkills />
+            <Layout>
+                <main class="container container--fluid margin-vert--lg">
+                    <ResumeExperience />
+                    <ResumeEducation />
+                    <ResumeSkills />
+                </main>
+            </Layout>
         </>
     )
 }
