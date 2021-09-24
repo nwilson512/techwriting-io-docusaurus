ee10cf99c8e4080cb760026423349a639974f57f
nathan <nathan.wilson.512@gmail.com>
2021-09-23 22:49:27 -0700

    clean up the email component code

--- src/components/HideEmail.js
+++ src/components/HideEmail.js
@@ -3,13 +3,9 @@ import React, { Component } from "react"
 function HideEmail() {
 
     function generateEmail() {
-        // I don't actually want to call this at render time. It may not be a good idea to make this a react component.
-        // I think this is what an event handler is for.
 
         // a caesar cipher will do for basic obfuscation
         const cipher = [110,  98, 106, 109, 117, 112,  59, 111,  98, 117, 105,  98, 111,  65, 117, 102, 100, 105, 120, 115, 106, 117, 106, 111, 104,  47, 106, 112 ];
-
-        // console.log(cipher);
 
         // iterate through the cipher array
         for (let index = 0; index < cipher.length; index++) {
@@ -27,9 +23,8 @@ function HideEmail() {
 
         // recombine array into string
         let decipher = cipher.join('');
-
-        // window.open(decipher, "_self");
         
+        // fire a link with the mailto
         window.location.href = decipher;
     }
 
