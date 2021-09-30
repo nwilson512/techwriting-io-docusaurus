d8a098ea6c7b51170e7ce1b21e5d34f09ff0bfa5
nathan <nathan.wilson.512@gmail.com>
2021-09-29 17:40:02 -0700

    move flex div out to avoid react className overwrite, this is hacky

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,26 +15,28 @@ function HomepageHeader() {
         {/* <Avatar /> */}
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons} class="container hero-buttons-container">
+        <div className={styles.buttons} class="container">
+        <div class="hero-buttons-container">
           <div class="hero-buttons">
-          <Link
-            className="button button--secondary button--lg margin--sm"
-            to="/resume">
-            View my resume
-          </Link>
-          <Link
-            className="button button--secondary button--lg margin--sm"
-            to="/sample-works">
-            See sample works
-          </Link>
-          </div>
-          <div class="hero-buttons">
-          <Link
-            className="button button--secondary button--lg margin--sm"
-            to="https://www.linkedin.com/in/nwilson512">
-            Find me on LinkedIn
-          </Link>
-          <HideEmail />
+            <Link
+              className="button button--secondary button--lg margin--sm"
+              to="/resume">
+              View my resume
+            </Link>
+            <Link
+              className="button button--secondary button--lg margin--sm"
+              to="/sample-works">
+              See sample works
+            </Link>
+            </div>
+            <div class="hero-buttons">
+            <Link
+              className="button button--secondary button--lg margin--sm"
+              to="https://www.linkedin.com/in/nwilson512">
+              Find me on LinkedIn
+            </Link>
+            <HideEmail />
+            </div>
           </div>
         </div>
       </div>
