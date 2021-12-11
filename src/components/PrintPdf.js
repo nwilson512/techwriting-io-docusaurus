a0f40881fd07a52789f3f1161985de672905a61c
nwilson512 <nathan.wilson.512@gmail.com>
2021-12-10 23:13:14 -0800

    attempt to clean up those functions and call them properly

--- src/components/PrintPdf.js
+++ src/components/PrintPdf.js
@@ -1,44 +1,38 @@
 import React from 'react';
 import ReactToPrint from 'react-to-print';
-
-// import ResumeEducation from './ResumeEducation';
-// import ResumeExperience from './ResumeExperience';
-// import ResumeSkills from './ResumeSkills';
-
 import ComponentToPrint from './ResumeAll';
 
-/* The way this currrently works is as follows:
-
-All of the resume components are collected into a single component called `ResumeAll`
-This component is the only component called on the `resume.js` page
-It looks like there's another way I could do this. If I want to call components separately, and apply different styles, 
-I can hide the components from this using the steps from the FAQ under the following section: `How do you make ComponentToPrint show only while printing`
-I'm not sure if there's any value in doing so. 
-
-I think I can also use the @media directive in css to define custom styling for when this is printed, meaning that I can control the style that way. 
-I should write this into a feature spec.
-*/ 
-
-// Get the current date and append it to the resume name automatically
-var today = new Date();
-
-const monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-
-var date = monthArray[today.getMonth()]+' '+today.getFullYear();
-
-// Detect chrome, I only plan to support printing on Chrome
-
-var sBrowser, sUsrAg = navigator.userAgent;
-var showButton = false;
-
-// conditionally returns the content + print button if user is running Chrome, otherwise it just returns the content
 class PrintPdf extends React.PureComponent {
-  render() {
+  
+  // Conditionally returns the content + print button if user is running Chrome, otherwise it just returns the content
+  CheckBrowser() {
+    var sUsrAg = navigator.userAgent;
+    var showButton = false;
+  
     if (sUsrAg.indexOf("Chrome") > -1) {
       showButton = true;
     }
-    if (showButton){
+    return(
+      showButton
+    );
+  }
+
+  // Gets the current date and append it to the resume name automatically
+  GetDate() {
+    var today = new Date();
+
+    const monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+    var date = monthArray[today.getMonth()]+' '+today.getFullYear();
+
+    return(
+      date
+    );
+  }
+
+  render() {
+    
+    if (this.CheckBrowser()){
     return (
       <div>
         <div class="print-button-wrapper">
@@ -53,7 +47,7 @@ class PrintPdf extends React.PureComponent {
             </button>;
           }}
           content={() => this.componentRef}
-          documentTitle={"Nathaniel Wilson " + date}
+          documentTitle={"Nathaniel Wilson " + this.GetDate()}
         />
         </div>
         <ComponentToPrint ref={el => (this.componentRef = el)} />
