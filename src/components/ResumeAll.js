a7e480e2b1f2356aa8d2da9c774a32cb95021e4d
nwilson512 <nathan.wilson.512@gmail.com>
2024-10-02 16:56:53 -0700

    rendering fixes for print to PDF

--- src/components/ResumeAll.js
+++ src/components/ResumeAll.js
@@ -1,30 +1,29 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
 import ResumeEducation from './ResumeEducation';
 import ResumeExperience from './ResumeExperience';
 import ResumeSkills from './ResumeSkills';
 
-
 class ResumeAll extends Component {
   render() {
     return (
-      <div class="print-wrapper">
-        <div class="row">
-          <div class="col col--10 col--offset-2 margin-bottom--md">
-            <h1 class="resume-title">
+      <div className="print-wrapper">
+        <div className="row">
+          <div className="col col--10 col--offset-2 margin-bottom--md print-resume-title">
+            <h1 className="resume-title">
               Nathaniel Wilson
             </h1>
           </div>
         </div>
-        <div class="row">
-          <div class="col col--2 print-title-col">
+        <div className="row">
+          <div className="col col--2 print-title-col">
             <ul>
-          <li class="print-email"><a href="https://techwriting.io">https://techwriting.io</a></li>
-          <li class="print-email"><a href="https://github.com/nwilson512">github.com/nwilson512</a></li>
-          </ul>
+              <li className="print-email"><a href="https://techwriting.io">https://techwriting.io</a></li>
+              <li className="print-email"><a href="https://github.com/nwilson512">github.com/nwilson512</a></li>
+            </ul>
           </div>
-          <div class="col row padding-horiz--none skills-list print-content-col">
-            <p class="col print-description">Degreed software technical writer with a decade of experience writing for developer audiences, practicing docs-as-code, performing front end web development, and accelerating everything with generative AI.</p>
+          <div className="col row padding-horiz--none skills-list print-content-col">
+            <p className="col print-description">Degreed software technical writer with a decade of experience writing for developer audiences, practicing docs-as-code, performing front end web development, and accelerating everything with generative AI.</p>
           </div>
         </div>
         <ResumeExperience />
